69472a29f9e6bf129f214dfb791ac4d30e5bd13c
Ilya Sher <ilya.sher@coding-knight.com>
2014-02-18 10:59:35 +0200

    packaging - add setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+setuptools.setup(
+    zip_safe=True,
+    name='cosmo-plugin-openstack-common',
+    version='0.1',
+    author='Ilya Sher',
+    author_email='ilya.sher@coding-knight.com',
+    packages=['cosmo_plugin_openstack_common'],
+    license='LICENSE',
+    description='Common code for Cosmo OpenStack plugins',
+    install_requires=[
+        "cosmo-plugin-common",
+        "python-novaclient",
+        "python-neutronclient",
+    ],
+    dependency_links=[
+        "https://github.com/Fewbytes/cosmo-plugin-common/tarball/" \
+        "master#egg=cosmo-plugin-common-0.1"
+    ]
+)
